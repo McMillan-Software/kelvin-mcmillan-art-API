f562d8cc66a759de801de6e6c1f384125a6b7ea8
Sam <sammcmillan97@gmail.com>
2025-04-19 15:05:15 +1200

    Set token expiry to be 30 days.

--- auth.py
+++ auth.py
@@ -20,7 +20,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(days=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     
