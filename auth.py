d9801a7513f06f367fb532d0eeab463815eb466d
Sam <sammcmillan97@gmail.com>
2026-01-18 12:49:20 +1300

    Added  mesage in case of .env issue

--- auth.py
+++ auth.py
@@ -16,6 +16,8 @@ env_path = base_dir / ".env"
 load_dotenv(dotenv_path=env_path)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not found! Check your .env file path.")
 ALGORITHM = os.getenv("ALGORITHM", "HS256") 
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
