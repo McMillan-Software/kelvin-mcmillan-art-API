10e3152a111aaf873d1497efe30bf035ea5e0596
Sam <sammcmillan97@gmail.com>
2026-01-26 16:15:09 +1300

    Added pages script

---
+++ sql/insert_pages.sql
@@ -0,0 +1,6 @@
+INSERT INTO pages (name) VALUES ("Marine");
+INSERT INTO pages (name) VALUES ("Rural");
+INSERT INTO pages (name) VALUES ("Landscape");
+INSERT INTO pages (name) VALUES ("Architectural");
+INSERT INTO pages (name) VALUES ("Potrait");
+INSERT INTO pages (name) VALUES ("Sport");
